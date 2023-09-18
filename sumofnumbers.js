3ca14e3cf8198046a68383faeef993f3ccbc8ed4
scn1901 <33670707+scn1901@users.noreply.github.com>
2023-09-17 19:41:16 -1000

    update lint
    recursion not working will ask TA

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,41 +1,42 @@
 /*
-		Name : Seri Nakamura
-    Assignment :
-    Date : 09.17
-    Time : Start: 19:00 End: 19:10
+Name : Seri Nakamura
+Assignment :
+Date : 09.17
+Time : Start: 19:00 End: 19:10
 */
 
 // Problem 1
-function sumFor(nums){
+function sumFor(nums) {
   let sum = 0;
-  for (let i = 0; i < nums.length; i++){
+  for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
   }
   return sum;
 }
 
-function sumWhile(nums){
-  let sum = 0, i = 0;
-  while (i < nums.length){
+function sumWhile(nums) {
+  let sum = 0;
+  let i = 0;
+  while (i < nums.length) {
     sum += nums[i];
     i++;
   }
   return sum;
 }
 
-function sumRecursion(nums){
-  if (nums.length === 0){
+function sumRecursion(nums) {
+  if (nums.length === 0) {
     return 0;
   } else {
-    return nums[0] + sumRecursion(nums.slice(1));
+    return (nums[0] + sumRecursion(nums.slice(1)));
   }
 }
 
-function sumTheSimpleWay(nums){
-  return _.reduce(nums, function(memo, num){ return memo + num }, 0);
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, num) { return memo + num; }, 0);
 }
 
-const nums = [1,2,3,4,5];
+const nums = [1, 2, 3, 4, 5];
 console.log(sumFor(nums));
 console.log(sumWhile(nums));
 console.log(sumRecursion(nums));
